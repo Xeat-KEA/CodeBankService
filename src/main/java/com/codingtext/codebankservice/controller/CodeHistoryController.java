b0a2b0e71c580eb92058827ad0e8c2c89baddc16
DaveLee-b <lpoln1855@gmail.com>
2024-11-03 16:35:12 +0900

    fix:swagger,hold feignclient

--- src/main/java/com/codingtext/codebankservice/controller/CodeHistoryController.java
+++ src/main/java/com/codingtext/codebankservice/controller/CodeHistoryController.java
@@ -2,6 +2,8 @@ package com.codingtext.codebankservice.controller;
 
 import com.codingtext.codebankservice.Dto.CodeHistoryDto;
 import com.codingtext.codebankservice.Service.CodeHistoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -9,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@Tag(name = "Code History API", description = "사용자의 문제 풀이 히스토리를 관리하는 API")
 @RestController
 @RequestMapping("/code/history")
 public class CodeHistoryController {
@@ -17,6 +19,7 @@ public class CodeHistoryController {
     private CodeHistoryService codeHistoryService;
 
     // 특정 유저의 히스토리 조회
+    @Operation(summary = "특정 유저의 히스토리 조회", description = "특정 유저의 문제 풀이 히스토리를 페이징하여 조회")
     @GetMapping("/{userId}")
     public ResponseEntity<List<CodeHistoryDto>> getUserHistory(
             @PathVariable Long userId,
@@ -28,7 +31,8 @@ public class CodeHistoryController {
     }
 
     // 유저가 컴파일 시 해당 문제를 히스토리에 저장 또는 갱신
-    @PostMapping("/{codeId}/compile")
+    @Operation(summary = "문제 풀이 히스토리 저장 또는 갱신", description = "유저가 문제를 컴파일하면 해당 문제를 히스토리에 저장하거나 이미 기록이있을경 시간 갱신")
+    @PostMapping("/compile/{codeId}")
     public ResponseEntity<String> updateHistory(
             @PathVariable Long codeId,
             @RequestHeader("UserId") Long userId, // 헤더로 UserId 받기
