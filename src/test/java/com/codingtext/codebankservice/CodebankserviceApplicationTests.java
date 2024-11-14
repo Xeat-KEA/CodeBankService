3ee9dd0d48a6f57dca5864dd7cd245c600d15d23
DaveLee-b <lpoln1855@gmail.com>
2024-11-14 13:20:18 +0900

    페인클라이언트 수정

--- src/test/java/com/codingtext/codebankservice/CodebankserviceApplicationTests.java
+++ src/test/java/com/codingtext/codebankservice/CodebankserviceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CodebankserviceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
