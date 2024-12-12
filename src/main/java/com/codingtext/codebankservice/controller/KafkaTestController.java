332bdd10b201fc1018b70de7188033ac116a4db1
DaveLee-b <lpoln1855@gmail.com>
2024-12-12 15:01:20 +0900

    kafka의존성주입

--- src/main/java/com/codingtext/codebankservice/controller/KafkaTestController.java
+++ src/main/java/com/codingtext/codebankservice/controller/KafkaTestController.java
@@ -1,16 +1,18 @@
 package com.codingtext.codebankservice.controller;
 
 import com.codingtext.codebankservice.kafka.KafkaProducerService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class KafkaTestController {
 
 
-    private KafkaProducerService kafkaProducerService;
+    private final KafkaProducerService kafkaProducerService;
 
     @GetMapping("/sendMessage")
     public String sendMessage(@RequestParam String topic, @RequestParam String key, @RequestParam String message) {
